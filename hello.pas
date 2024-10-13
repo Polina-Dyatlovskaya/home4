f329ee665befe096ac4675e0ec439ab2baa33f91
Polina Dyatlovskaya <dyatlowskaya.polina@yandex.ru>
2024-10-13 21:46:21 +0300

    Add greetings in different languages

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  
+  var language := ReadString('Введите название языка, на котором вы говорите (русский, english,  deutsch):');
+  if language = 'русский' then
+    Print('Здравствуйте!')
+  else if language = 'english' then
+    Print('Hello!')
+  else if language = 'deutsch' then
+    Print('Hallo!')
+  else
+    Print('Извините, я не знаю такого языка :(');
 end.
